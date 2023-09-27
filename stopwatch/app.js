bceafadbeee22d2049b1403ef75c7f2d58844375
yamnahafeez12345 <128935056+yamnahafeez12345@users.noreply.github.com>
2023-09-27 19:38:30 +0500

    Add files via upload

---
+++ stopwatch/app.js
@@ -0,0 +1,42 @@
+let timeDisplay = document.querySelector('.timeDisplay');
+let stop = document.getElementById('stop');
+let start = document.getElementById('start');
+let Reset = document.getElementById('Reset');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
+let timerId = null;
+start.addEventListener('click', function(){
+    
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+   timerId = setInterval(startTimer,10);
+});
+
+stop.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+Reset.addEventListener('click' , function(){
+    clearInterval(timerId);
+    timeDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 00;
+});
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+            mins++;
+        }
+    }
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+  
+timeDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+}
